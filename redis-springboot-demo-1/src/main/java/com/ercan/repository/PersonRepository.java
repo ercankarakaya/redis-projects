dd3c7952712b2171d8f76072af2d759d698ef5ad
ercankarakaya <karakayaercan71@gmail.com>
2024-07-20 00:34:18 +0300

    Person repository interface created.

--- redis-springboot-demo-1/src/main/java/com/ercan/repository/PersonRepository.java
+++ redis-springboot-demo-1/src/main/java/com/ercan/repository/PersonRepository.java
@@ -1,2 +1,9 @@
-package com.ercan.repository;public interface PersonRepository {
+package com.ercan.repository;
+
+import com.ercan.model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, String> {
 }
