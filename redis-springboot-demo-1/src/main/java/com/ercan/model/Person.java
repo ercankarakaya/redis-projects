45d8cd74cd209b5ac34039b090a3827aa495e255
ercankarakaya <karakayaercan71@gmail.com>
2024-07-20 00:26:34 +0300

    Person model created

--- redis-springboot-demo-1/src/main/java/com/ercan/model/Person.java
+++ redis-springboot-demo-1/src/main/java/com/ercan/model/Person.java
@@ -1,4 +1,19 @@
 package com.ercan.model;
 
-public class Person {
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@RedisHash("Person")
+@Data
+public class Person implements Serializable {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
 }
