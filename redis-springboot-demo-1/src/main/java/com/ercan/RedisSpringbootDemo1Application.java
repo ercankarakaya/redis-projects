6499c69571eb6796102fcf3656f2a9c8cb7b9123
ercankarakaya <karakayaercan71@gmail.com>
2024-07-20 00:50:34 +0300

    Redis example runner

--- redis-springboot-demo-1/src/main/java/com/ercan/RedisSpringbootDemo1Application.java
+++ redis-springboot-demo-1/src/main/java/com/ercan/RedisSpringbootDemo1Application.java
@@ -1,14 +1,32 @@
 package com.ercan;
 
+import com.ercan.model.Person;
+import com.ercan.repository.PersonRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisSpringbootDemo1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedisSpringbootDemo1Application.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringbootDemo1Application.class, args);
+    }
 
-	}
+    @Bean
+    public ApplicationRunner runner(PersonRepository personRepository) {
+        return args -> {
+            Person person = new Person();
+            person.setId("123ABC");
+            person.setFirstName("John");
+            person.setLastName("Wick");
+            person.setAge(40);
+
+            personRepository.save(person);
+            Person personSaved = personRepository.findById("123ABC").orElse(null);
+            System.out.println(personSaved);
+        };
+    }
 
 }
