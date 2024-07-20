736e4474c17bec6d0c4d2805908d1680acec40a2
ercankarakaya <karakayaercan71@gmail.com>
2024-07-20 17:58:42 +0300

    Redis demo 2 initial commit

---
+++ redis-springboot-demo-2/src/main/java/com/ercan/RedisSpringbootDemo2Application.java
@@ -0,0 +1,13 @@
+package com.ercan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringbootDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringbootDemo2Application.class, args);
+	}
+
+}
---
+++ redis-springboot-demo-2/src/test/java/com/ercan/RedisSpringbootDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ercan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringbootDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
